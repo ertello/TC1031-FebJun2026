dc6233e98be5dc2930d5f13a52e9c86c98167277
Eduardo Rodriguez-Tello <ertello@cinvestav.mx>
2026-03-06 09:39:14 -0600

    Update sesion 8 - Algoritmos de ordenamiento parte 2

---
+++ 08-Ordenamiento-parte02/SortAlgorithms.h
@@ -0,0 +1,174 @@
+#ifndef _SORT_ALGORITHMS_H_
+#define _SORT_ALGORITHMS_H_
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <random>
+#include <chrono>
+
+template <class T>
+class SortAlgorithms {
+  private:
+    // semilla del generador de numeros aleatorios (time-based)
+    unsigned seed; 
+    // generador de numeros aleatorios (Mersenne Twister 64 bits)
+    std::mt19937_64 gen;
+
+    void merge(std::vector<T> &A, size_t low, size_t m, size_t high, size_t &compara);
+
+  public:
+    SortAlgorithms();
+    ~SortAlgorithms();
+    void randomShuffle(std::vector<T> &A);
+    void printVector(std::vector<T> A);
+    
+    void swapSort(std::vector<T> &A, size_t n, size_t &compara, size_t &swap);
+    void selectionSort(std::vector<T> &A, size_t n, size_t &compara, size_t &swap);
+    void bubbleSort(std::vector<T> &A, size_t n, size_t &compara, size_t &swap);
+    void insertionSort(std::vector<T> &A, size_t n, size_t &compara, size_t &swap);
+    void mergeSort(std::vector<T> &A, size_t low, size_t high, size_t &compara);
+};
+
+// O(1)
+template<class T>
+SortAlgorithms<T>::SortAlgorithms() {
+    // semilla del generador de numeros aleatorios (time-based)
+    seed = std::chrono::system_clock::now().time_since_epoch().count();
+    // generador de numeros aleatorios (Mersenne Twister 64 bits)
+    std::mt19937_64 gen;
+    // inicializar el generador con la semilla creada
+    gen.seed(seed);
+}
+
+template<class T>
+SortAlgorithms<T>::~SortAlgorithms() {}
+
+// Mezcla los elementos del vector en forma aleatoria
+// O(n)
+template<class T>
+void SortAlgorithms<T>::randomShuffle(std::vector<T> &A) {
+    std::shuffle(A.begin(), A.end(), gen);
+}
+
+// Imprime en pantalla los elementos del vector
+// O(n)
+template<class T>
+void SortAlgorithms<T>::printVector(std::vector<T> A) {
+    std::cout << "Los elementos del vector ordenado son: " << std::endl;
+    for(size_t i = 0; i < A.size(); i++) {
+      std::cout << A[i] << " ";
+    }
+    std::cout << std::endl;
+}
+
+// O(n^2) en el peor caso
+template<class T>
+void SortAlgorithms<T>::swapSort(std::vector<T> &A, size_t n, size_t &compara, size_t &swap) {
+    compara = swap = 0;
+    for (size_t i = 0; i < n-1; i++) {
+        for (size_t j = i + 1; j < n; j++) {
+            compara++;
+            if (A[i] > A[j]) {
+                std::swap(A[i], A[j]);
+                swap++;
+            }
+        }
+    }
+}
+
+// O(n^2) en el peor caso
+template<class T>
+void SortAlgorithms<T>::selectionSort(std::vector<T> &A, size_t n, size_t &compara, size_t &swap) {
+    compara = swap = 0;
+    for (size_t i = 0; i < n-1; i++) {
+        size_t minIndex = i;
+        for (size_t j = i + 1; j < n; j++) {
+            compara++;
+            if (A[j] < A[minIndex])
+                minIndex = j;
+        }
+        std::swap(A[i], A[minIndex]);
+        swap++;
+    }
+}
+
+// O(n^2) en el peor caso
+template<class T>
+void SortAlgorithms<T>::bubbleSort(std::vector<T> &A, size_t n, size_t &compara, size_t &swap) {
+    compara = swap = 0;
+    for (size_t i = 0; i < n; i++) {
+        for (size_t j = 0; j < n-i-1; j++) {
+            compara++;
+            if (A[j] > A[j+1]) {
+                std::swap(A[j], A[j+1]);
+                swap++;
+            }
+        }
+    }
+}
+
+// O(n^2) en el peor caso
+template<class T>
+void SortAlgorithms<T>::insertionSort(std::vector<T> &A, size_t n, size_t &compara, size_t &swap) {
+    compara = swap = 0;
+    for (size_t i = 1; i < n; i++) {
+        T key = A[i];
+        size_t j = i;
+        while (j > 0 && A[j-1] > key) {
+            A[j] = A[j-1];
+            j--;
+            compara++;
+            swap++;
+        }
+        A[j] = key;
+    }
+}
+
+// O(n)
+template<class T>
+void SortAlgorithms<T>::merge(std::vector<T> &A, size_t low, size_t m, size_t high, size_t &compara) {
+    size_t i, j, k;
+    // calcular el tamaño de los vectores L y R
+    size_t n1 = m - low + 1;
+    size_t n2 = high - m;
+    // Crear los vectores auxiliares L y R
+    std::vector<T> L(n1); 
+    std::vector<T> R(n2);
+    for (i = 0; i < n1; i++) L[i] = A[low + i];
+    for (j = 0; j < n2; j++) R[j] = A[m + 1 + j];
+    // Fusionar los vectores L y R de forma ordenada
+    i = j = 0;
+    k = low;
+    while (i < n1 && j < n2) {
+        compara++;
+        if (L[i] <= R[j]) {
+            A[k] = L[i];
+            i++;
+        }
+        else {
+            A[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    // Copiar elementos restantes de L y R, si existen
+    while (i < n1) { A[k] = L[i]; i++; k++; }
+    while (j < n2) { A[k] = R[j]; j++; k++; }
+}
+
+// O(n log n) peor caso
+template<class T>
+void SortAlgorithms<T>::mergeSort(std::vector<T> &A, size_t low, size_t high, size_t &compara) {
+    if (low < high) {
+        // encontrar el punto medio
+        size_t m = low + (high - low) / 2;
+        // Divide el problema en dos partes
+        mergeSort(A, low, m, compara);
+        mergeSort(A, m+1, high, compara);
+        // Fusionar ambas partes ordenadas
+        merge(A, low, m, high, compara);
+    }
+}
+
+#endif   // _SORT_ALGORITHMS_H_
---
+++ 08-Ordenamiento-parte02/main.cpp
@@ -0,0 +1,131 @@
+/*
+ * Algoritmos de ordenamiento parte 02
+ *
+ * Compilación con debug:
+ *  g++ -std=c++17 *.cpp -Wall -g -o main
+ *
+ * Compilación para ejecucion:
+ *  g++ -std=c++17 *.cpp -Wall -O3 -o main
+ *
+ * Ejecución con redireccion (input and output):
+ *  ./main < TestCases/test01.txt
+ *  ./main < TestCases/test01.txt > output01.txt
+ */
+
+#include <chrono>
+#include <iostream>
+#include "SortAlgorithms.h"
+
+using std::cin;
+using std::cout;
+using std::endl;
+
+int main(void) {
+    int flag;
+    size_t i, size, comparaciones, swaps;
+    cin >> size;
+    cout << "Size: " << size << endl;
+    cin >> flag;
+    cout << "Print flag: " << flag << endl;
+    std::vector<int> myVector(size);
+    // Lectura de los elementos del vector
+    for (i = 0; i < size; i++) {
+        cin >> myVector[i];
+    }
+    if (flag) {
+        cout << "Los elementos del vector son: " << endl;
+        for (i = 0; i < size; i++) {
+            cout << myVector[i] << " ";
+        }
+        cout << endl;
+    }
+    // Instancia un objeto de la clase AlgoritmoSort
+    SortAlgorithms<int> sortObj;
+    // Mezcla los elementos del vector en forma aleatoria
+    sortObj.randomShuffle(myVector);
+    // Ordenamiento del vector utilizando swap sort
+    // Inicio conteo de tiempo de ejecución
+    auto startTime = std::chrono::high_resolution_clock::now();
+    cout << "Swap sort" << endl;
+    comparaciones = swaps = 0;
+    sortObj.swapSort(myVector, size, comparaciones, swaps);
+    if (flag)
+        sortObj.printVector(myVector);
+    // Termina conteo de tiempo de ejecución
+    auto endTime = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+    cout << "\tComparaciones: " << comparaciones << endl;
+    cout << "\tIntercambios: " << swaps << endl;
+    cout << "\tTiempo de ejecución en microsegundos: " << duration.count() << endl; 
+    // Mezcla los elementos del vector en forma aleatoria
+    sortObj.randomShuffle(myVector);
+
+    // Ordenamiento del vector utilizando selection sort
+    // Inicio conteo de tiempo de ejecución
+    startTime = std::chrono::high_resolution_clock::now();
+    cout << "Selection sort" << endl;
+    comparaciones = swaps = 0;
+    sortObj.selectionSort(myVector, size, comparaciones, swaps);
+    if (flag)
+        sortObj.printVector(myVector);
+    // Termina conteo de tiempo de ejecución
+    endTime = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+    cout << "\tComparaciones: " << comparaciones << endl;
+    cout << "\tIntercambios: " << swaps << endl;
+    cout << "\tTiempo de ejecución en microsegundos: " << duration.count() << endl; 
+    // Mezcla los elementos del vector en forma aleatoria
+    sortObj.randomShuffle(myVector);
+    
+    // Ordenamiento del vector utilizando bubble sort
+    // Inicio conteo de tiempo de ejecución
+    startTime = std::chrono::high_resolution_clock::now();
+    cout << "Bubble sort" << endl;
+    comparaciones = swaps = 0;
+    sortObj.bubbleSort(myVector, size, comparaciones, swaps);
+    if (flag)
+        sortObj.printVector(myVector);
+    // Termina conteo de tiempo de ejecución
+    endTime = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+    cout << "\tComparaciones: " << comparaciones << endl;
+    cout << "\tIntercambios: " << swaps << endl;
+    cout << "\tTiempo de ejecución en microsegundos: " << duration.count() << endl; 
+    // Mezcla los elementos del vector en forma aleatoria
+    sortObj.randomShuffle(myVector);
+
+    // Ordenamiento del vector utilizando insertion sort
+    // Inicio conteo de tiempo de ejecución
+    startTime = std::chrono::high_resolution_clock::now();
+    cout << "Insertion sort" << endl;
+    comparaciones = swaps = 0;
+    sortObj.insertionSort(myVector, size, comparaciones, swaps);
+    if (flag)
+        sortObj.printVector(myVector);
+    // Termina conteo de tiempo de ejecución
+    endTime = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+    cout << "\tComparaciones: " << comparaciones << endl;
+    cout << "\tIntercambios: " << swaps << endl;
+    cout << "\tTiempo de ejecución en microsegundos: " << duration.count() << endl; 
+    // Mezcla los elementos del vector en forma aleatoria
+    sortObj.randomShuffle(myVector);
+
+    // Ordenamiento del vector utilizando merge sort
+    // Inicio conteo de tiempo de ejecución
+    startTime = std::chrono::high_resolution_clock::now();
+    cout << "Merge sort" << endl;
+    comparaciones = swaps = 0;
+    sortObj.mergeSort(myVector, 0, size-1, comparaciones);
+    if (flag)
+        sortObj.printVector(myVector);
+    // Termina conteo de tiempo de ejecución
+    endTime = std::chrono::high_resolution_clock::now();
+    duration = std::chrono::duration_cast<std::chrono::microseconds>(endTime - startTime);
+    cout << "\tComparaciones: " << comparaciones << endl;
+    cout << "\tTiempo de ejecución en microsegundos: " << duration.count() << endl; 
+    // Mezcla los elementos del vector en forma aleatoria
+    sortObj.randomShuffle(myVector);
+
+    return 0;
+}
